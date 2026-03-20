2abfdfa5552f3e66d2acfb2504842f21cc2af64c
Curtis Delicata <curtis.delicata@outlook.com>
2026-03-20 14:32:10 +0000

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,11 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
+use App\Models\Team;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
-use App\Models\User;
-use App\Models\Team;
 
 class UserSeeder extends Seeder
 {
@@ -15,21 +16,22 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+
+        $adminPassword = Str::random(12);
         $adminUser = User::create([
             'name' => 'Admin User',
             'email' => 'admin@example.com',
-            'password' => Hash::make('password'),
+            'password' => Hash::make($adminPassword),
             'email_verified_at' => now(),
         ]);
-        $adminUser->assignRole('admin');
-        // $this->createTeamForUser($adminUser);
-    }
 
-    private function createTeamForUser($user)
-    {
-        $team = Team::first();
-        $team->users()->attach($user);
-        $user->current_team_id = 1;
-        $user->save();
+        $team = Team::firstOrFail();
+        $adminUser->teams()->syncWithoutDetaching([$team->id]);
+
+        $role = Role::where('name', 'super_admin')->firstOrFail();
+        $adminUser->assignRole($role);
+
+        // Print passwords to console
+        echo "Admin password: {$adminPassword}\n";
     }
 }
